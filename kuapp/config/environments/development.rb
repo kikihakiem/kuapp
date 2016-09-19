615a65d51b549dac65a37141a9a4bc9c6c3210ae
Kiki <kiki.hakiem@gmail.com>
2016-09-19 13:05:56 +0700

    Whitelist docker host's IP

--- kuapp/config/environments/development.rb
+++ kuapp/config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Whitelist docker host's IP
+  config.web_console.whitelisted_ips = '172.17.0.1/16'
 end
